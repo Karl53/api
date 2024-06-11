4dd164097438b1029584278848d4697f748898c5
Karl53 <107112499+Karl53@users.noreply.github.com>
2024-06-11 23:37:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 // Fonction pour générer une chaîne alphanumérique aléatoire de longueur spécifiée
+<h1>Beetles</h1>
+<h2>External morphology</h2>
+<h3>Head</h3>
+<h4>Mouthparts</h4>
+<h3>Thorax</h3>
+<h4>Prothorax</h4>
+<h4>Pterothorax</h4>
+
 function generateRandomString($length = 6) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $randomString = '';
